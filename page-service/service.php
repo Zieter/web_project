97018a7bad2646324dcd8b0ae70f45d20af5c0e0
Zieter <103229713+Zieter@users.noreply.github.com>
2022-07-25 00:13:13 +0800

    Add files via upload

---
+++ page-service/service.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>伊餃-手工水餃貼心服務</title>
+        <!-- Bootstrap icons-->	    
+		<link href="css/servicestyle2.css" rel="stylesheet">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+		<script src="js/service_js.js"></script>
+		<script src="js/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+<div class="Box">
+    <!---------------------上框-------------------->
+	<div class="middle_background"></div>
+    <div class="menu" id="menu" >
+        <div class="menu_left">
+        <!--預留-->
+        </div>
+        <div class="menu_list">
+            <div class="menu_list_item" id="menu_list_item"><a href="../page-news/news.php">最新消息</a></div>
+            <div class="menu_list_item" id="menu_list_item"><a href="../page-product/product.php">手工水餃</a></div>
+            <div class="menu_list_item" ><a href="../page-Home/Home.php"><img class="LOGO" src="img/上框/LOGO-裁切.png"></a></div>
+            <div class="menu_list_item" id="menu_list_item"><a href="../page-service/service.php">貼心服務</a></div>
+            <div class="menu_list_item" id="menu_list_item"><a href="../page-notice/notice.php">購物須知</a></div>
+            </div>
+        <!---------------------↓MOBILE漢堡--------------------->
+        <div class="hamburger_menu_area" id="hamburger_menu_area">
+            <div class="hamburger_menu_area_top">
+                <div class="hamburger_cancel"><img  id="hamburger_cancel_img" onclick="document.getElementById('hamburger_menu_area').style.visibility='hidden'" src="img/上框/cancel.png"></div>
+                <div class="hamburger_none"></div><!--預留-->
+            <div class="hamburger_menber">
+                <div class="hamburger_membership_left"><!--預留--></div>
+			    <div class="hamburger_membership_icon" ><a href="../phpmember/member.php"><img src="img/上框/392531_account_friend_human_man_member_icon.png" alt=""></a></div>
+                <div class="hamburger_membership_icon" ><a href="#"><img src="img/上框/3844432_magnifier_search_zoom_icon.png"  alt=""></a></div>
+                <div class="hamburger_membership_right"><!--預留--></div>
+			</div>
+          </div>
+          <div class="hamburger_menu_list"><!--Mobile-->
+            <div class="hamburger_menu_list_item" id="menu_list_item_RWD"><a href="../page-Home/Home.php">首&emsp;&emsp;頁</a></div>
+            <hr>
+            <div class="hamburger_menu_list_item" id="menu_list_item_RWD"><a href="../page-news/news.php">最新消息</a></div>
+            <hr>
+            <div class="hamburger_menu_list_item" id="menu_list_item_RWD"><a href="../page-product/product.php">手工水餃</a></div>
+            <hr>
+            <div class="hamburger_menu_list_item" id="menu_list_item_RWD"><a href="../page-service/service.php">貼心服務</a></div>
+            <hr>
+            <div class="hamburger_menu_list_item" id="menu_list_item_RWD"><a href="../page-notice/notice.php">購物須知</a></div>
+          </div>
+        </div>
+
+        <!---------------------↑MOBILE漢堡--------------------->
+        <div class="membership">
+            <div class="membership_left"><a ><img  id="hamburger" onclick="document.getElementById('hamburger_menu_area').style.visibility='visible'" src="img/上框/hamburger.png"  alt=""></a></div>
+            <div class="membership_icon" >
+                <a href="../phpmember/member.php"><img src="img/上框/392531_account_friend_human_man_member_icon.png" alt=""></a>
+            </div>
+            <div class="membership_icon" ><a href="#"><img src="img/上框/3844432_magnifier_search_zoom_icon.png"  alt=""></a></div>
+            <div class="membership_right"><!--預留--></div>
+		</div>
+
+
+	</div><!--menu-->
+
+        <!------------------中框-------------------->
+    <div class="main_area" >
+        <div class="container_out" id="container_out">
+		    <h3 style="color:white;">●美味水餃建議煮法</h3>
+		    <hr style="color:#fff ;">
+		<section>
+		    <div class="container_in" >
+			<div class="main-timeline" >
+			  <div class="timeline left">
+				<div class="card">
+				  <div class="card-body ">
+					<h3>Step 1<img src="img/中框/step1.png" align=left></h3>
+					<p class="mb-0">將熱水燒開至水滾。</p>
+					
+				  </div>
+				</div>
+			  </div>
+			  <div class="timeline right">
+				<div class="card">
+				  <div class="card-body ">
+					<h3>Step 2<img src="img/中框/step2.png"  align=right></h3>
+					<p class="mb-0">從冷凍庫取出水餃，不須退冰，直接放入滾水之鍋內</p>
+				  </div>
+				</div>
+			  </div>
+			  <div class="timeline left">
+				<div class="card">
+				  <div class="card-body ">
+					<h3>Step 3<img src="img/中框/step3.png" align=left></h3>
+					<p class="mb-0">用鍋鏟輕輕推水餃</p>
+				  </div>
+				</div>
+			  </div>
+			  <div class="timeline right">
+				<div class="card">
+				  <div class="card-body ">
+					<h3>Step 4<img src="img/中框/step4.png" align=right></h3>
+					<p class="mb-0">(1)水滾後開至中小火，並加入冷水</p>
+					<p class="mb-0">(2)再次水滾後，第二次加入冷水</p>
+					<p class="mb-0">(3)再次水滾後，第三次加入冷水</p>
+				  </div>
+				</div>
+			  </div>
+			  <div class="timeline left">
+				<div class="card">
+				  <div class="card-body ">
+					<h3>Step 5<img src="img/中框/step5.png" align=left></h3>
+					<p class="mb-0">美味水餃上桌!</p>
+				  </div>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</section>
+	    </div>
+    </div><!--main_area-->
+
+        <!---------------------下框-------------------->
+        
+
+    <div class="footer_area">
+    
+        <div class="footer_area_left">
+            <img id="LOGO_footer" src="img/下框/LOGO-裁切.png">
+        </div>
+        <div class="footer_area_center">
+            <div class="footer_area_top">
+                <div class="block" id="block01"><img src="img/下框/圖示/32711_history_time_clock_icon.png" width="30px" height="30px"><span>營業時間</span></div>
+                <div class="block" id="block02"><img src="img/下框/圖示/icon-line.png" width="25px" height="25px"><span>LINE</span></div>
+                <div class="block" id="block03"><img src="img/下框/圖示/4975305_google_google map_location_location place_map_icon.png" width="25px" height="25px"><span>地址</span></div>
+                <div class="block" id="block04">週一至週五9:00~16:00</div>
+                <div class="block" id="block05">ID:789654123</div>
+                <div class="block" id="block06">328桃園市觀音區中山路一段1159號之1號</div>
+            </div>
+            <div class="footer_area_bottom"> 
+                <div class="A_block"><a href="../page-news/news.php"><span>最新消息</span></a></div>
+                <div class="A_block"><a href="../page-product/product.php"><span>手工水餃</span></a></div>
+                <div class="A_block"><a href="../page-Home/Home.php"><span>首頁</span></a></div>
+                <div class="A_block"><a href="../page-service/service.php"><span>貼心服務</span></a></div>
+                <div class="A_block"><a href="../page-notice/notice.php"><span>購物須知</span></a></div>
+            </div>
+        </div>
+        <div class="footer_area_right"></div>
+    </div><!--footer_area-->
+
+
+
+
+
+</div><!--box-->
+
+
+    
+		
+	
+	<!----------------------返回top按鈕------------------->
+		<div class="backtop">top</div>
+	
+
+	
+</body>
+</html>
